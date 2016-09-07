43ee79205a42e9236e16ef5b4147a3eeea06e757
Brian Hudson <brianjasonhudson@gmail.com>
2016-09-07 13:19:41 -0500

    "Altered getUsers in controller to return only data"

--- controller.js
+++ controller.js
@@ -1,8 +1,8 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService){
     var cPromise = mainService.getUsers();
     cPromise.then(function(results) {
-      $scope.users = results;
-      console.log(results)
+      $scope.users = results.data;
+      console.log($scope.users)
     })
 
 })
